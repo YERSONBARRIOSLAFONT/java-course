732b9f5f88425a678a3dbab52dc9f9150aacc3ce
YERSONBARRIOSLAFONT <barrioslafont.5@gmail.com>
2024-06-22 23:58:54 -0500

    First commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+
+    }public static void main(String[] args) {
+        System.out.println("Bienvenid@ a Screen Match");
+        System.out.println("Pelicula: Matrix");
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true;
+        double notaDeLaPelicula = 9.8;
+        double media = 8.433333333333334;
+        System.out.println(media);
+}
+
+
+
+
