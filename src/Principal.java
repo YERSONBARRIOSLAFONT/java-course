fe328d8db5e94d3c317c367f687fba2ebfb6f00d
YERSONBARRIOSLAFONT <barrioslafont.5@gmail.com>
2024-06-24 22:38:40 -0500

    New commit

---
+++ src/Principal.java
@@ -0,0 +1,26 @@
+public class Principal {
+    public static void main(String[] args) {
+        Pelicula miPelicula = new Pelicula();
+        miPelicula.nombre = "Encanto";
+        miPelicula.fechaDeLanzamiento = 2021;
+        miPelicula.duracionEnMinutos = 120;
+
+        miPelicula.muestraFichaTecnica();
+        miPelicula.evalua(7.8);
+        miPelicula.evalua(10);
+
+        System.out.println(miPelicula.sumaDeLasEvaluaciones);
+        System.out.println(miPelicula.totalDeLasEvaluaciones);
+        System.out.println(miPelicula.calculaMedia());
+
+        //System.out.println("Mi otra pelicula es: " + otraPelicula.nombre);
+        //System.out.println("Fecha de lanzamiento: " + otraPelicula.fechaDeLanzamiento);
+
+        Pelicula otraPelicula = new Pelicula();
+        otraPelicula.nombre = "Matrix";
+        otraPelicula.fechaDeLanzamiento = 1998;
+        otraPelicula.duracionEnMinutos = 188;
+
+        otraPelicula.muestraFichaTecnica();
+    }
+}
